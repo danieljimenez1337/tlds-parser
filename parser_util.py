9012c85483ea867fcb33261c838885f5f65d9a7e
djimene3 <djimene3@uci.edu>
2019-01-15 14:00:14 -0800

    add example json and add more functions

--- parser_util.py
+++ parser_util.py
@@ -40,6 +40,7 @@ class Line(BoundaryBox):
         super().__init__(x,y,width,height)
         self.words = []
         self.__text=""
+        self.isolated = True
     
     def addText(self,text: str):
         self.__text += text + " "
@@ -74,12 +75,29 @@ class ParsedImage():
     
     def __init__(self,jsonFileName:str) -> None:
         self.pages = convertJson(jsonFileName)
+        self.checkLineIsolation()
     
     def printPages(self):
         for page in self.pages:
             for region in page.regions:
                 for line in region.lines:
-                    print(line.getText())
+                    if line.isolated:
+                        print(line.getText())
+    
+    #checks if lines are isolated
+    def checkLineIsolation(self):
+        for page in self.pages:
+            for region in page.regions:
+                for line in region.lines:
+                    if line.isolated:
+                        for page2 in self.pages:
+                            for region2 in page2.regions:
+                                for line2 in region2.lines:
+                                    if checkBoundaryBoxCollision(line,line2):
+                                        line.isolated=False
+                                        line2.isolated=False
+
+
                     
 
 def convertBB(BoundaryBox) -> (int,int,int,int):  
@@ -135,3 +153,22 @@ def convertDataForKmeans(pages: List[Page]) -> np.ndarray:
                     templist.append(lineaverageheight/len(line.words))
                     dataset.append(templist)
     return np.array(dataset)
+
+def percentError(actual: float, expected: float) -> float:
+    return abs((actual-expected)/expected)*100
+
+def checkBoundaryBoxCollision(box1: BoundaryBox ,box2: BoundaryBox)->bool:
+    b1W = box1.getWidth()
+    b1H = box1.getHeight()
+    b1X = box1.getX()
+    b1Y = box1.getY()
+
+    b2W = box2.getWidth()
+    b2H = box2.getHeight()
+    b2X = box2.getX()
+    b2Y = box2.getY()
+
+    if ((b1X < (b2X + b2W)) and ((b1X + b1W) > b2X) and (b1Y < (b2Y + b2H)) and ((b1Y + b1H) > b2Y)):
+        return True
+    else:
+        return False
