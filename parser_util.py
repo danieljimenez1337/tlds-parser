666d552c771e41143971548a5ec100af6788db6b
khoitd1997 <khoidinhtrinh@gmail.com>
2019-01-19 21:51:29 -0800

    refactor: Moved json parsing into object creation
    The list of regions, lines, words only get created once, thus, should be created on object creation and accessed on getter only

--- parser_util.py
+++ parser_util.py
@@ -8,11 +8,13 @@ from typing import List
 
 
 class BoundingBox():
-    def __init__(self, x: int, y: int, width: int, height: int) -> None:
-        self.__x = x
-        self.__y = y
-        self.__width = width
-        self.__height = height
+    def __init__(self, rawBoundingBox: dict) -> None:
+        boundingDimension = rawBoundingBox["boundingBox"]
+        bb = list(boundingDimension.split(","))
+        self.__x = int(bb[0])
+        self.__y = int(bb[1])
+        self.__width = int(bb[2])
+        self.__height = int(bb[3])
 
     def getX(self) -> int:
         return self.__x
@@ -28,20 +30,25 @@ class BoundingBox():
 
 
 class Word(BoundingBox):
-    def __init__(self, text: str, x: int, y: int, width: int, height: int) -> None:
-        super().__init__(x, y, width, height)
-        self.__text = text
+    def __init__(self, rawWord: dict) -> None:
+        super().__init__(rawWord)
+        self.__text = rawWord["text"]
 
     def getText(self) -> str:
         return self.__text
 
 
 class Line(BoundingBox):
-    def __init__(self, x: int, y: int, width: int, height: int) -> None:
-        super().__init__(x, y, width, height)
-        self.words = []
+    def __init__(self, rawLine: dict) -> None:
+        super().__init__(rawLine)
+        self.__words = []
         self.__text = ""
         self.isolated = True
+
+        for word in rawLine["words"]:
+            wordObject = Word(word)
+            self.__words.append(wordObject)
+            self.addText(wordObject.getText())
 
     def addText(self, text: str) -> None:
         if self.__text == "":
@@ -55,19 +62,31 @@ class Line(BoundingBox):
     def getCharWidth(self) -> float:
         return self.getWidth()/len(self.__text)
 
+    def getWords(self) -> List[Word]:
+        return self.__words
+
 
 class Region(BoundingBox):
-    def __init__(self, x: int, y: int, width: int, height: int) -> None:
-        super().__init__(x, y, width, height)
-        self.lines = []
+    def __init__(self, rawRegion: dict) -> None:
+        super().__init__(rawRegion)
+        self.__lines = []
+
+        for line in rawRegion["lines"]:
+            self.__lines.append(Line(line))
+
+    def getLines(self) -> List[Line]:
+        return self.__lines
 
 
 class Page():
-    def __init__(self, language: str, textAngle: float, orientation: str) -> None:
-        self.regions = []
-        self.__language = language
-        self.__textAngle = textAngle
-        self.__orientation = orientation
+    def __init__(self, rawPage: dict) -> None:
+        self.__regions = []
+        self.__language = rawPage["language"]
+        self.__textAngle = float(rawPage["textAngle"])
+        self.__orientation = rawPage["orientation"]
+
+        for region in rawPage['regions']:
+            self.__regions.append(Region(region))
 
     def getLanguage(self) -> str:
         return self.__language
@@ -78,24 +97,35 @@ class Page():
     def getOrientation(self) -> str:
         return self.__orientation
 
+    def getRegions(self) -> List[Region]:
+        return self.__regions
+
 
 class ParsedImage():
 
     def __init__(self, jsonFileName: str) -> None:
-        self.pages = convertJson(jsonFileName)
+        self.__pages = []
+
+        with open(jsonFileName) as jsonData:
+            data = (json.load(jsonData))
+            for page in data["pages"]:
+                self.__pages.append(Page(page))
 
     def printPages(self) -> None:
-        for page in self.pages:
-            for region in page.regions:
-                for line in region.lines:
+        for page in self.__pages:
+            for region in page.getRegions():
+                for line in region.getLines():
                     print(line.getText())
+
+    def getPages(self) -> List[Page]:
+        return self.__pages
 
     def getText(self) -> dict:
         paragraphs = []
         paragraph = []
-        for page in self.pages:
-            for region in page.regions:
-                for line in region.lines:
+        for page in self.__pages:
+            for region in page.getRegions():
+                for line in region.getLines():
                     if lineIsIndented(region, line):
                         paragraphs.append(paragraph)
                         paragraph = [line.getText()]
@@ -119,42 +149,6 @@ def lineIsIndented(region, line)->bool:
         return False
 
 
-def convertBoundingBox(boundingBoxes: List) -> (int, int, int, int):
-    bb = list(boundingBoxes.split(","))
-    x = int(bb[0])
-    y = int(bb[1])
-    width = int(bb[2])
-    height = int(bb[3])
-    return (x, y, width, height)
-
-
-def convertJson(jsonFileName: str) -> List[Page]:
-    with open(jsonFileName) as jsonData:
-        data = (json.load(jsonData))
-
-    pages = []
-    for page in data["pages"]:
-        language = page["language"]
-        textAngle = float(page["textAngle"])
-        orientation = page["orientation"]
-        pageObject = Page(language, textAngle, orientation)
-        pages.append(pageObject)
-        for region in page['regions']:
-            x, y, width, height = convertBoundingBox(region["boundingBox"])
-            regionObject = Region(x, y, width, height)
-            pageObject.regions.append(regionObject)
-            for line in region["lines"]:
-                x, y, width, height = convertBoundingBox(line["boundingBox"])
-                lineObject = Line(x, y, width, height)
-                regionObject.lines.append(lineObject)
-                for word in line["words"]:
-                    x, y, width, height = convertBoundingBox(word["boundingBox"])
-                    wordObject = Word(word["text"], x, y, width, height)
-                    lineObject.words.append(wordObject)
-                    lineObject.addText(wordObject.getText())
-    return pages
-
-
 def checkBoundingBoxCollision(box1: BoundingBox, box2: BoundingBox)->bool:
     b1W = box1.getWidth()
     b1H = box1.getHeight()
@@ -175,19 +169,19 @@ def checkBoundingBoxCollision(box1: BoundingBox, box2: BoundingBox)->bool:
 def convertDataForKmeans(pages: List[Page]) -> np.ndarray:
     dataset = []
     for page in pages:
-        for region in page.regions:
-                for line in region.lines:
+        for region in page.getRegions():
+                for line in region.getLines():
                     tempList = []
                     lineAverageWidth = 0
                     lineAverageHeight = 0
                     characterCount = 0
-                    for word in line.words:
+                    for word in line.getWords():
                         lineAverageWidth += word.width
                         lineAverageHeight += word.height
                         characterCount += len(word.text)
 
                     tempList.append(lineAverageWidth/characterCount)
-                    tempList.append(lineAverageHeight/len(line.words))
+                    tempList.append(lineAverageHeight/len(line.getWords()))
                     dataset.append(tempList)
     return np.array(dataset)
 
